4c59db45482b7404d715fb6707c490943771e82f
Marianne Linhares Monteiro <marianne.monteiro@ccc.ufcg.edu.br>
2016-05-05 11:40:49 -0300

    Update code.cpp

--- Algorithms/String/HASH/code.cpp
+++ Algorithms/String/HASH/code.cpp
@@ -2,53 +2,46 @@
 
 #define MOD 1000000007
 #define BASE 257
-#define MAX 600400
+#define MAX 1000400
 #define ll long long int
+#define pb push_back
 
 using namespace std;
 
-ll aux[MAX];
+ll aux[MAX], hash1[MAX], hash2[MAX];
 int n, m;
-set<ll> st;
+string s1, s2;
+ll inverse;
+
+ll power(ll num, ll pow) {
+    if (pow < 1) return 1;
+    ll result = power(num, pow/2);
+    result = (result * result) % MOD;
+    if (pow % 2)
+        result = (result * num) % MOD;
+	return result;
+}
 
 void preHash() {
-	aux[0] = 1;
+    aux[0] = 1;
     for(int i = 1; i < MAX; i++) aux[i] = aux[i-1] * BASE % MOD;
 }
 
-ll buildHash(string s){
-    ll ans = 0;
-    int l = s.length();
-    for(int i = 0; i < l; i++) ans = (ans * BASE + s[i]) % MOD;
-    return ans;
-}
-
-bool verify(string s){
-    int l = s.length();
-    ll s_hash = buildHash(s);
-    for(int i = 0; i < l; i++){
-        for(int c = 'a'; c <= 'c'; c++){
-            if(s[i] == c) continue;
-            if(st.find((((c-s[i]) * aux[l-i-1] + s_hash) % MOD + MOD) % MOD) !=st.end())
-                 return true;
-        }
-    }
-    return false;
+// remove the i-element of hash1
+void remove(int i) {
+	ll aux_hash = ((((hash1[s1.size()] - hash1[i] + MOD) % MOD) * inverse) % MOD + hash1[i-1]) % MOD;
 }
 
 int main() {
 
-	preHash();
-	string s;
-    scanf("%d %d",&n, &m);
-    for(int i = 0; i < n ; i++){
-        cin >> s;
-        st.insert(buildHash(s));
-    }
-    for(int i = 0; i < m; i++){
-        cin >> s;
-		printf("%s\n",verify(s)?"YES":"NO");
-    }
+    preHash();
+    
+    inverse = power(BASE, MOD-2);
+    
+    // calculating the hash
+    cin >> s1;
+    hash1[0] = 0;
+    for(int i = 1 ; i <= s1.size(); i++) hash1[i] = (hash1[i-1] + (s1[i-1] - 'a') * aux[i-1]) % MOD;
 
-    return 0;
+     return 0;
 }
