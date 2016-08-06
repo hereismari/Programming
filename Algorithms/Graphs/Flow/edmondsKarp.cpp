7fa8d9a33132ae9dde8aeaa8b449edbdaecfc880
Marianne Linhares Monteiro <marianne.monteiro@ccc.ufcg.edu.br>
2016-08-06 18:46:14 -0300

    Update edmondsKarp.cpp

--- Algorithms/Graphs/Flow/edmondsKarp.cpp
+++ Algorithms/Graphs/Flow/edmondsKarp.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
-/*
-To be verified!
-*/
- 
- 
+
+using namespace std;
+
 #define INF 1e9
 #define MAXN 100
-#define min(a,b) a < b ? a : b
+#define ll long long int
 
-int res[MAXN][MAXN], n, s, t;
-int p[MAXN], dist[MAXN];
+ll cap[MAXN][MAXN];
+int n, s, t;
+int p[MAXN];
+ll dist[MAXN];
  
-int augpath(int v, int mf) {
+ll augpath(int v, ll mf) {
   
   if(v == s) return mf;
   else if (p[v] != -1) {
-  	mf = augpath(p[v], min(mf, res[p[v]][v]));
-  	res[p[v]][v] -= mf;
-  	res[v][p[v]] += mf;
+  	mf = augpath(p[v], min(mf, cap[p[v]][v]));
+  	cap[p[v]][v] -= mf;
+  	cap[v][p[v]] += mf;
   	return mf;
   }
   
@@ -27,7 +27,8 @@ int augpath(int v, int mf) {
 void bfs() {
 	
 	memset(p, -1, sizeof p);
-	memset(dist, INF, sizeof dist);
+	for(int i = 0; i < MAXN; i++) dist[i] = INF;
+	
 	dist[s] = 0;
 	
 	queue<int> q;
@@ -36,49 +37,69 @@ void bfs() {
 	while(!q.empty()) {
 		int u = q.front(); q.pop();
 		if (u == t) break;
-		for(int v = 0; v < MAXN; v++)
-			if(res[u][v] > 0 && dist[v] == INF) {
+		for(int v = 0; v < MAXN; v++) {
+			if(cap[u][v] > 0 && dist[v] == INF) {
 				dist[v] = dist[u] + 1;
 				q.push(v);
 				p[v] = u;
 			}
+		}
 	}
 }
 
  
-int maxflow(int so, int si){
+ll maxflow(int so, int si){
   
   s = so; t = si;
   
-  int flow = 0;
+  ll flow = 0;
   while(true) {
   
     // BFS
     bfs();
     
     // augment path and stop condition
-    int f = augpath(t, INF);
-    if(!f) return flow;
+    ll f = augpath(t, INF);
+    if(!f) break;
     flow += f;
   }
+	
+  return flow;
 }
- 
+
+void add(int u, int v, int c) {
+	cap[u][v] += c;
+	cap[v][u] += c;
+}
+
+void clean_graph() {
+	for(int i = 0; i < MAXN; i++)
+		for(int j = 0; j < MAXN; j++) {
+			cap[i][j] = 0;
+		}
+}
+
+
+int conv(char c) {
+	if (isupper(c)) return c - 'A';
+	return c - 'a' + 27;
+}
+
 int main() {
-    
-    int n, e, a, b, c;
-    while(scanf("%d %d", &n, &e) != EOF && (n || e)) {
-	    memset(res, 0, sizeof res);
-	    res[0][n] = res[n-1][2*n-1] = INF;
-	    for(int i = 0; i < n-2; i++){
-	      scanf("%d %d", &a, &c); a--;
-	      res[a][n+a] = c;
-	    }
-	    
-	    for(int i = 0; i < e; i++) {
-	      scanf("%d %d %d", &a, &b, &c); a--; b--;
-	      res[n+a][b] = res[n+b][a] = c;
-	    }
-	    
-	    printf("%d\n", maxflow(0, 2*n - 1));
-    }
+	
+	int e, u, v, c;
+	char a, b;
+	
+	scanf("%d", &e);
+	
+	clean_graph();
+	
+	for(int i = 0; i < e; i++) {
+		scanf("\n%c %c %d", &a, &b, &c);
+		u = conv(a); v = conv(b);
+		if(u != v) add(u, v, c);
+	}
+	
+	printf("%lld\n", maxflow(0, 25));
+	return 0;
 }
